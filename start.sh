d6d45673656bea920b8cc1a2cd5df7c1194d1e05
Joshua Hamilton <joshua@tinydog.io>
2023-03-03 14:20:34 -0500

    Update to allow for individual repos.

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 ORGANIZATION=$ORGANIZATION
+REPO=$REPO
 ACCESS_TOKEN=$ACCESS_TOKEN
-
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+if [ -z "$REPO" ]; then
+  REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+  ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+else
+  REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}"   https://api.github.com/repos/${ORGANIZATION}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+  ./config.sh --url https://github.com/${ORGANIZATION}/${REPO} --token ${REG_TOKEN}
+fi
 
 cleanup() {
   echo "Removing runner..."
